f3015f0a732f8ec06695884bc4d5aa6ca60a64e5
15matias15 <matpereirag@gmail.com>
2018-12-21 22:12:25 +0100

    assignment 4

---
+++ src/Assignment/Assignment4.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const ValidationComponent = (props) => {
+    return (
+        <div hidden={props.textLength === 0}>
+            {
+                props.textLength < 5 ?
+                    <p>Text too short</p> :
+                    <p>Text long enough</p>
+            }
+        </div>
+    )
+};
+
+const CharComponent = (props) => {
+    return (
+        <div
+            style={{
+                display: 'inline-block',
+                padding: 16,
+                textAlign: 'center',
+                margin: 16,
+                border: '1px solid black'
+            }}>
+            {
+                props.textChar.map((ltr, index) => {
+                    return <p key={index} onClick={() => props.delete(index)}>{ltr}</p>
+                })
+            }
+        </div>
+    )
+};
+
+class App extends Component {
+    state = {
+        text: ''
+    }
+
+    inputHandler = (event) => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    deleteHandler = (ltrIndex) => {
+        const text = this.state.text.split('');
+        text.splice(ltrIndex, 1);
+        this.setState({
+            text: text.join('')
+        })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <input type="text" onChange={this.inputHandler} value={this.state.text} />
+                <p>{this.state.text.length}</p>
+                <ValidationComponent textLength={this.state.text.length} />
+                <CharComponent
+                    textChar={this.state.text.split('')}
+                    delete={this.deleteHandler} />
+            </div>
+        );
+    }
+}
+
+export default App;
